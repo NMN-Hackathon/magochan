2f7dff011a0bcc84d5ccffa0f2aa5b9580ad1799
ayumi.goto <ayumi.goto@jp.oro.com>
2014-06-07 17:33:19 +0900

    背景合わせ、閲覧者が切断された際の処理

--- js/view.js
+++ js/view.js
@@ -5,12 +5,12 @@ $(function() {
 	var myPeerId = null;
 	var myPeer = null;
 	var SPACE_KEY = 32;
-
+	
 	// ビデオ、オーディオの準備
 	setupMediaStream(function(stream, streamUrl) {
 		var url = URL.createObjectURL(stream);
 		mediaStream = stream;
-		$("#myVideo").attr('src', url);
+		$("#video0").attr('src', url);
 		initializedMediaStream(stream);
 		setInterval(updateViewers, 5000);
 	});
@@ -18,10 +18,12 @@ $(function() {
 	// 閲覧者の表示を更新する
 	function updateViewers() {
 		getViewers(function(viewerIds) {
+			viewerIds.sort();
 			// vieweridの数だけ生成する
 			for(var i = 0; i < viewerIds.length; i++) {
 				viewerId = viewerIds[i];
-				if (viewerId != MagochanPeerId && viewerId != myPeerId) {
+				// if (viewerId != MagochanPeerId && viewerId != myPeerId) {
+				if (viewerId != MagochanPeerId) {
 					//　閲覧者が既にあったらなにもしない
 					if (0 < $("video[data-viewer-id=" + viewerId + "]").length) {
 						continue;
@@ -33,10 +35,19 @@ $(function() {
 						// 閲覧者受信
 						call.on('stream', function(stream) {
 							var url = URL.createObjectURL(stream);
-							var $viewerVideo = $("<video width='160' height='120' autoplay></video>");
-							$viewerVideo.attr('src', url);
-							$viewerVideo.attr('data-viewer-id', viewerId);
-							$("#video_area").append($viewerVideo);
+							$emptyVideos = $("video.viewVideo").filter(function() {
+								return !$(this).attr('src');
+							});
+							// srcが空のやつを探す
+							if (0 < $emptyVideos.length) {
+								$viewerVideo = $($emptyVideos.get(0));
+								$viewerVideo.attr('src', url);
+								$viewerVideo.attr('data-viewer-id', viewerId);
+							}
+						});
+						call.on('close', function() {
+							$viewerVideo = $("video.viewVideo[data-viewer-id=" + viewerId + "]");
+							$viewerVideo.attr('src', '');
 						});
 					})(viewerId);
 				}
@@ -64,6 +75,10 @@ $(function() {
 		peer.on('error', function(error) {
 			console.log('Peerの作成に失敗');
 			console.log(error);
+		});
+		peer.on('close', function() {
+			console.log('peer close');
+			peer.destroy();
 		});
 		// 受信者の接続が来たら、mediaStreamの応答を返す
 		peer.on('call', function(call) {
