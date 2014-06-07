502d6a184e1334ba94891022a7598bade55a89e4
katashin <ktsn55@gmail.com>
2014-06-07 13:24:51 +0900

    create PhotoSlide module.

--- js/photoslide.js
+++ js/photoslide.js
@@ -1,1 +1,22 @@
-/* photoslide.js */
+(function(window, document, $) {
+
+var _Config = {
+    kTransitionTimeMs: 60000,
+    kPhotoDirectory: "./img/photoslide/"
+};
+
+var PhotoSlide = function(container_selector) {
+    this.$container = $(container_selector);
+};
+
+$.extend(PhotoSlide.prototype, {
+    startSlideShow: function() {
+
+    },
+
+    stopSlideShow: function() {
+
+    }
+});
+
+})(window, document, jQuery);
